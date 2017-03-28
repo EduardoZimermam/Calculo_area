f9e79f1ede3197f03a824dd99a83ef315ff8d078
Eduardo Zimermam <eduardozimerman@live.com>
2017-03-28 16:11:50 -0300

    Modificando o problema e resolvendo erros de compilação

--- areaGeom.java
+++ areaGeom.java
@@ -1,66 +1,64 @@
 import java.util.Scanner;
 
-class formGeo
-class circulo{
+class formGeo{
 
+	double area;
 	Scanner in = new Scanner(System.in);
 
-	double leCirc(){
-		double raio;
+	class circulo extends formGeo{
 
-		System.out.println("\n\n\n\n\nDigite o raio do circulo: ");
-		raio = in.nextDouble();
+		double leCirc(){
+			double raio;
 
-		return(raio);
+			System.out.println("\n\n\n\n\nDigite o raio do circulo: ");
+			raio = in.nextDouble();
+
+			return(raio);
+		}
+
+		void areaCirc(double raio){
+			double area = Math.PI *(raio*raio);
+		}
 	}
 
-	void areaCirc(double raio){
-		double pi = 3.1416;
-		double area = pi *(raio*raio);
-	}
-}
+	class quadrado extends formGeo{
 
-class quadrado{
-
-	Scanner in = new Scanner(System.in);
-
-	double leQuad(){
-		double lado;
+		double leQuad(){
+			double lado;
 	
-		System.out.println("\n\n\n\n\nDigite o lado do quadrado: ");
-		lado = in.nextDouble();
+			System.out.println("\n\n\n\n\nDigite o lado do quadrado: ");
+			lado = in.nextDouble();
 
-		return(lado);
+			return(lado);
+		}
+
+		void areaQuad(double lado){
+
+			double area = lado * lado;
+		}
 	}
 
-	void areaQuad(double lado){
+	class retangulo extends formGeo{
 
-		double area = lado * lado;
+		double areaRet(double base, double altura){
+
+			return(base*altura);
+		}
+
+		void leRet(){
+			
+			double base, altura, area;
+
+			System.out.println("\n\n\n\n\nDigite a base do retangulo: ");
+			base = in.nextDouble();
+
+			System.out.println("\nDigite o lado do quadrado: ");
+			altura = in.nextDouble();
+
+			area = areaRet(base, altura);
+		}	
 	}
 }
-
-class retangulo{
-
-	Scanner in = new Scanner(System.in);
-
-	double areaRet(double base, double altura){
-
-		return(base*altura);
-	}
-
-	void leRet(){
-		double base, altura, area;
-
-		System.out.println("\n\n\n\n\nDigite a base do retangulo: ");
-		base = in.nextDouble();
-
-		System.out.println("\nDigite o lado do quadrado: ");
-		altura = in.nextDouble();
-
-		area = areaRet(base, altura);
-	}	
-}
-
 
 class areaGeo{
 	public static void main(String args[]){
@@ -76,18 +74,24 @@ class areaGeo{
 			System.out.println("Digite a opção desejada:");
 			opcao = in.nextInt();
 
-			if (opcao == 1){
-				circulo objCirc = new circulo();
+			switch (opcao){
+				case 1:
+					formGeo objCirc = new circulo();
 
-				objCirc.areaCirc(objCirc.leCirc());
-			}else if (opcao == 2){
-				quadrado objQuad = new quadrado();
+					break;
 
-				objQuad.areaQuad(objQuad.leQuad());
-			}else if (opcao == 3){
-				retangulo objRet = new retangulo();
+				case 2:
+					formGeo objQuad = new quadrado();
 
-				objRet.leRet();
+					break;
+
+				case 3:
+					formGeo objRet = new retangulo();
+
+					break;
+
+				default:
+					println("Opção inválida!!");
 			}
 		}
 	}
